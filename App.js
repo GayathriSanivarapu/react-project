2c7c59931ab2f64901cfb07c85182adb04f81ebe
GayathriSanivarapu <pandusanivarapu9@gmail.com>
2023-05-16 11:12:04 +0530

    react-comment

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heloo every one</h1>
+      <h2>Hii This is gayathri</h2>
+
+    </div>
+  );
+}
+
+export default App;
